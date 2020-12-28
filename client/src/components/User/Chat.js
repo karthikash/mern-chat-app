9a40020166a6effc6e5f7dcae5661c20b0094c5d
Karthik Ashokkumar <karthikashokumar@gmail.com>
2020-12-29 01:06:38 +0530

    Chat UI

--- client/src/components/User/Chat.js
+++ client/src/components/User/Chat.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
-import { Button, Card, CardBody, CardSubtitle, Col, Form, FormGroup, Input, ListGroup, ListGroupItem, Media, Row } from "reactstrap";
+import { Button, Card, CardBody, CardSubtitle, Col, Container, Form, FormGroup, Input, ListGroup, ListGroupItem, Media, Row } from "reactstrap";
 import { Constants } from '../../config';
+import $ from 'jquery';
+import moment from 'moment';
 
 class Chat extends Component {
 
@@ -17,6 +19,14 @@ class Chat extends Component {
             status: '',
             message: '',
             error: ''
+        },
+        selectedUser: {
+            activeIndex: '',
+            userId: '',
+            name: '',
+            username: '',
+            dp: '',
+            lastseen: ''
         }
     });
 
@@ -42,14 +52,28 @@ class Chat extends Component {
             });
     }
 
-    handleClick = (e) => {
-        e.preventDefault();
-        console.log(e);
+    isActive = (index, user) => {
+        this.setState({
+            selectedUser: {
+                activeIndex: index,
+                userId: user._id,
+                name: `${user.sFirstName} ${user.sLastName}`,
+                username: user.sUsername,
+                dp: user.sImage,
+                lastseen: user.createdAt
+            }
+        });
+    }
+
+    uploadButton = () => {
+        $('input[type=file]').trigger('click');
     }
 
     render() {
-        const users = this.state.userList.map((user) => {
-            return <ListGroupItem key={user._id} onClick={this.handleClick}>
+        const { activeIndex, name, dp, lastseen } = this.state.selectedUser;
+        const users = this.state.userList.map((user, index) => {
+            const className = activeIndex === index ? 'active' : '';
+            return <ListGroupItem className={className} key={user._id} onClick={this.isActive.bind(this, index, user)}>
                 <Row>
                     <Col md="3">
                         <span className="pull-left">
@@ -78,17 +102,46 @@ class Chat extends Component {
                     <Col md="8">
                         <Card className="chat-card">
                             <CardBody>
-                                <CardSubtitle tag="h5" className="mb-2 text-muted">Username</CardSubtitle>
-                                <Form>
+                                <CardSubtitle className="text-muted">
+                                    <Row>
+                                        <Col md="1">
+                                            <span className="pull-left">
+                                                <Media className="dp" src={dp} />
+                                            </span>
+                                        </Col>
+                                        <Col md="11" className="pull-left">
+                                            <Container>
+                                                <Row>
+                                                    {name}
+                                                </Row>
+                                                <Row>
+                                                    <small><em>{lastseen ? 'last seen ' + moment(lastseen).fromNow() : null}</em></small>
+                                                </Row>
+                                            </Container>
+                                        </Col>
+                                    </Row>
+                                </CardSubtitle>
+                                <hr />
+                                <Form className="chat-form">
                                     <Row>
                                         <Col md="10">
                                             <FormGroup>
                                                 <Input type="text" id="message" name="message" />
                                             </FormGroup>
                                         </Col>
-                                        <Col md="2">
+                                        <Col md="1">
                                             <FormGroup>
-                                                <Button color="primary" block>Send</Button>
+                                                <Button onClick={this.uploadButton}>
+                                                    <i className="fa fa-camera" aria-hidden="true"></i>
+                                                </Button>
+                                                <Input type="file" id="media" name="media" accept="image/*, video/*" style={{ display: "none" }} />
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md="1">
+                                            <FormGroup>
+                                                <Button color="primary">
+                                                    <i className="fa fa-paper-plane" aria-hidden="true"></i>
+                                                </Button>
                                             </FormGroup>
                                         </Col>
                                     </Row>
--- client/src/components/User/Profile.js
+++ client/src/components/User/Profile.js
@@ -3,6 +3,7 @@ import { Button, Card, CardBody, CardImg, CardSubtitle, CardTitle, Col, Form, Fo
 import { isAlpha } from "validator";
 import { Constants } from '../../config';
 import moment from "moment";
+import $ from 'jquery';
 
 class Profile extends Component {
 
@@ -10,7 +11,6 @@ class Profile extends Component {
         super(props);
 
         this.state = this.getInitialState();
-
     }
 
     getInitialState = () => ({
@@ -23,6 +23,9 @@ class Profile extends Component {
             sUsername: '',
             createdAt: '',
             updatedAt: '',
+        },
+        files: {
+            dp: ''
         },
         errors: {},
         response: {
@@ -41,8 +44,15 @@ class Profile extends Component {
             errors: {
                 ...this.state.errors,
                 [e.target.name]: ''
+            },
+            files: {
+                dp: e.target.files[0]
             }
         });
+        //if (e.target.files[0]) {
+        //} else {
+        //$('.select').attr('disabled', true); 
+        //}/
     }
 
     validate = () => {
@@ -113,14 +123,63 @@ class Profile extends Component {
         }
     }
 
+    uploadProfilePic = () => {
+        const { files } = this.state;
+        const _id = localStorage.getItem('_id');
+        const token = localStorage.getItem('token');
+        const data = new FormData();
+        data.append('_id', _id);
+        data.append('dp', files.dp);
+        const httpHeaders = {
+            'Authorization': `Bearer ${token}`
+        }
+        const options = {
+            method: 'PATCH',
+            headers: new Headers(httpHeaders),
+            body: data
+        };
+        fetch(`${Constants.REACT_APP_API_BASE_URL}/user/upload/dp`, options)
+            .then((success) => {
+                return window.location.reload();
+            })
+            .catch((error) => {
+                console.log('error: ', error);
+            });
+        this.setState(this.getInitialState());
+    }
+
+    uploadButton = () => {
+        $('input[type=file]').trigger('click');
+    }
+
+    isFileSelected = () => {
+        const { dp } = this.state.files;
+        console.log(dp);
+        if (dp) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     render() {
         const { user, errors, response } = this.state;
         return (
             <div>
                 <Row>
                     <Col md="4">
-                        <Card className="edit-profile-card">
-                            <CardImg top width="100%" src={user.sImage} alt={user.sFirstName} />
+                        <Card className="edit-profile-card" style={{ borderColor: "white" }}>
+                            <CardImg top width="100%" src={user.sImage} alt="Change your avatar" style={{ borderRadius: "50%" }} />
+                            <br />
+                            <Row className="justify-content-center">
+                                <Col md="5">
+                                    <Button color="secondary" onClick={this.uploadButton} block>Choose</Button>
+                                    <Input type="file" accept="image/*" multiple={false} style={{ display: "none" }} onChange={this.handleChange} />
+                                </Col>
+                                <Col md="5">
+                                    <Button color="secondary" onClick={this.uploadProfilePic} block disabled={!this.isFileSelected()}>Upload</Button>
+                                </Col>
+                            </Row>
                             <CardBody>
                                 <CardTitle tag="h5">{`${user.sFirstName} ${user.sLastName}`}</CardTitle>
                                 <CardSubtitle tag="h6" className="mb-2 text-muted"><i>@{user.sUsername}</i></CardSubtitle><br />
@@ -129,7 +188,7 @@ class Profile extends Component {
                         </Card>
                     </Col>
                     <Col md="8">
-                        <Card className="edit-profile-card">
+                        <Card className="edit-profile-card" style={{ borderColor: "white" }}>
                             <CardBody>
                                 <CardTitle tag="h5" className="text-center text-muted">Edit Profile</CardTitle>
                                 <Form onSubmit={this.handleSubmit}>
